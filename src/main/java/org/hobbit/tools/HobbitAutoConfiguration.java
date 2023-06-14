6f8c04273c95d17e1075774141b002f202b0f39d
lhy <liuhongyu0131@163.com>
2023-06-14 20:56:15 +0800

    屏蔽错误

--- src/main/java/org/hobbit/tools/HobbitAutoConfiguration.java
+++ src/main/java/org/hobbit/tools/HobbitAutoConfiguration.java
@@ -5,6 +5,7 @@ import static org.springframework.web.context.WebApplicationContext.SCOPE_REQUES
 import org.hobbit.tools.jackson.convert.ConvertCache;
 import org.hobbit.tools.jackson.convert.IConvert;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
@@ -12,10 +13,11 @@ import org.springframework.context.annotation.Scope;
  * @author lhy
  * @version 1.0.0 2023/6/14
  */
-@SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection", "SpringFacetCodeInspection"})
+@SuppressWarnings({"SpringFacetCodeInspection"})
 @AutoConfiguration
 public class HobbitAutoConfiguration {
 
+  @ConditionalOnBean(IConvert.class)
   @Scope(SCOPE_REQUEST)
   @Bean
   public ConvertCache convertCache(IConvert convert) {
